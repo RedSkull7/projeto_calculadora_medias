348893801676db29c15d2f368025cf0ee1ecc2e7
Icaro <icaro.nata147@gmail.com>
2023-01-23 20:32:27 -0300

    fix: limitação de casas decimais e nota minima

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ function adicionaLinha(){
 
     linhas += linha;
     }
-    
+
     inputNomeAtividade.value ='';
     inputNotaAtividade.value ='';
 }
@@ -48,8 +48,8 @@ function atualizaTabela(){
 function atualizaMediaFinal(){
     const mediaFinal = calculaMediaFinal();
 
-    document.getElementById('media-final-valor').innerHTML = mediaFinal
-    document.getElementById('media-final-resultado').innerHTML = mediaFinal >= 7 ? spanAprovado : spanReprovado;
+    document.getElementById('media-final-valor').innerHTML = mediaFinal.toFixed(2)
+    document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
 }
 
 function calculaMediaFinal(){
